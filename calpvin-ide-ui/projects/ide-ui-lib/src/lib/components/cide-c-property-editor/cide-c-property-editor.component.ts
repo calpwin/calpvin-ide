3edbb958b032e95c9beb78f6ab7e19c1d837131a
irotaev <irotaev@gmaill.com>
2020-02-10 17:06:45 +0300

    dev

---
+++ calpvin-ide-ui/projects/ide-ui-lib/src/lib/components/cide-c-property-editor/cide-c-property-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-cide-c-property-editor',
+  templateUrl: './cide-c-property-editor.component.html',
+  styleUrls: ['./cide-c-property-editor.component.css']
+})
+export class CideCPropertyEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- calpvin-ide-ui/projects/ide-ui-lib/src/lib/ide-ui-lib.module.ts
+++ calpvin-ide-ui/projects/ide-ui-lib/src/lib/ide-ui-lib.module.ts
@@ -4,12 +4,14 @@ import { EventManagerService } from './services/event-manager.service';
 import { VirtualFileTreeService } from './services/virtual-tree.service';
 import { DevModuleManagerService } from './services/dev-module-manager.service';
 import { WorkspaceService } from './services/workspace.service';
+import { CideCPropertyEditorComponent } from './components/cide-c-property-editor/cide-c-property-editor.component';
 
 
 
 @NgModule({
   declarations: [
-    CideComponentDirective],
+    CideComponentDirective,
+    CideCPropertyEditorComponent],
   imports: [
   ],
   exports: [CideComponentDirective]
